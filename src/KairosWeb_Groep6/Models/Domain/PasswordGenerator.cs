176add691eccf22877c4dcf6eeffe1906a44ad91
DimmyMaenhout <DimmyMaenhout@DIMMYMAENHOD09C>
2017-02-19 22:10:58 +0100

    PasswordGenerator toegevoegd

---
+++ src/KairosWeb_Groep6/Models/Domain/PasswordGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KairosWeb_Groep6.Models.Domain
+{
+    public class PasswordGenerator
+    {
+        public static string GeneratePassword(int length)
+        {
+            string randomstring = "";
+            Random random = new Random();
+            char[] charArray = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz0123456789".ToCharArray();
+
+            for (var i = 0; i < length; i++)
+            {
+                int index = random.Next(0, charArray.Length);
+                char kar = charArray[index];
+                randomstring += kar;
+            }
+            return randomstring;
+        }
+    }
+}
