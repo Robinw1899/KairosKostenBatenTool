ee3a88280488ca16d4596c14635fa8c768193416
robin <robin@ROBIN>
2017-02-19 12:39:44 +0100

    JobcoachRepo

--- src/KairosWeb_Groep6/Data/Repositories/JobcoachRepository.cs
+++ src/KairosWeb_Groep6/Data/Repositories/JobcoachRepository.cs
@@ -3,34 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using KairosWeb_Groep6.Models.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace KairosWeb_Groep6.Data.Repositories
 {
     public class JobcoachRepository : IJobcoachRepository
     {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly DbSet<Jobcoach> _jobcoaches;
+
+        public JobcoachRepository(ApplicationDbContext _context)
+        {
+            _dbContext = _context;
+            _jobcoaches = _context.Jobcoaches;
+        }
         public void AddJobcoach(Jobcoach jobcoach)
         {
-            throw new NotImplementedException();
+            _jobcoaches.Add(jobcoach);
         }
 
         public IEnumerable<Jobcoach> GetAll()
         {
-            throw new NotImplementedException();
+            return _jobcoaches.ToList();//Hier moeten nog meer includes bij naarmate we andere kolommen hebben in de DB
         }
 
         public Jobcoach GetById(int id)
         {
-            throw new NotImplementedException();
+            return _jobcoaches.SingleOrDefault(b => b.JobcoachId == id);//include
         }
 
         public void RemoveJobcoach(Jobcoach jobcoach)
         {
-            throw new NotImplementedException();
+            _jobcoaches.Remove(jobcoach);
         }
 
         public void SaveChanges()
         {
-            throw new NotImplementedException();
+            _dbContext.SaveChanges();
         }
     }
 }
