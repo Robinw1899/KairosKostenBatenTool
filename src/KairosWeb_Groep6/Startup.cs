346295948d479a9814a6989d8a50ecfcc6d6353a
Thomas Aelbrecht <thomasaelbrecht@live.com>
2017-02-19 18:40:14 +0100

    ApplicationInsights verwijderd

--- src/KairosWeb_Groep6/Startup.cs
+++ src/KairosWeb_Groep6/Startup.cs
@@ -28,9 +28,6 @@ namespace KairosWeb_Groep6
             {
                 // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
                 builder.AddUserSecrets();
-
-                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
-                builder.AddApplicationInsightsSettings(developerMode: true);
             }
 
             builder.AddEnvironmentVariables();
@@ -43,7 +40,6 @@ namespace KairosWeb_Groep6
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddApplicationInsightsTelemetry(Configuration);
 
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
