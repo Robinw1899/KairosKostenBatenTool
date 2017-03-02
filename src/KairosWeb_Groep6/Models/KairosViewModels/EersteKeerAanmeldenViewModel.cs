fb033cf74f6f754cacd184728f7f9640d595e87f
Thomas Aelbrecht <thomasaelbrecht@live.com>
2017-03-02 08:34:18 +0100

    EersteKeerAanmelden vertaalfout

--- src/KairosWeb_Groep6/Models/KairosViewModels/EersteKeerAanmeldenViewModel.cs
+++ src/KairosWeb_Groep6/Models/KairosViewModels/EersteKeerAanmeldenViewModel.cs
@@ -1,17 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KairosWeb_Groep6.Models.KairosViewModels
 {
     public class EersteKeerAanmeldenViewModel
     {
+        [HiddenInput]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Gelieve je wachtwoord in te vullen")]
         [Display(Name = "Wachtwoord")]
         [StringLength(16, ErrorMessage = "Het {0} moet minstens {2} en mag maximum {1} karakters lang zijn.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Required(ErrorMessage = "Gelieve je wachtwoord te bevestigen")]
         [DataType(DataType.Password)]
         [Display(Name = "Bevestig wachtwoord")]
         [Compare("Password", ErrorMessage = "Het wachtwoord en bevestigingswachtwoord komen niet overeen.")]
