adf891fbf067aa0363c16685d4d6d7a23d1e9e29
Thomas Aelbrecht <thomasaelbrecht@live.com>
2017-03-04 20:50:03 +0100

    InfrastructuurKost afgewerkt

--- src/KairosWeb_Groep6/Models/Domain/Kosten/InfrastructuurKost.cs
+++ src/KairosWeb_Groep6/Models/Domain/Kosten/InfrastructuurKost.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace KairosWeb_Groep6.Models.Domain.Kosten
+﻿namespace KairosWeb_Groep6.Models.Domain.Kosten
 {
     public class InfrastructuurKost : KostOfBaat
     {
@@ -8,6 +6,8 @@ namespace KairosWeb_Groep6.Models.Domain.Kosten
         public int Id { get; set; }
         public Soort Soort { get; set; }
         public Type Type { get; set; }
+        public string Beschrijving { get; set; }
+        public double Bedrag { get; set; }
         #endregion
 
         #region Constructors
