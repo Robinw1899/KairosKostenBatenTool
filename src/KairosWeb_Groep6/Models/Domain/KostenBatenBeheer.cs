27d38403e35715dab1f4aec68650d08eaa59341a
Thomas Aelbrecht <thomasaelbrecht@live.com>
2017-03-04 15:03:53 +0100

    Methoden om totale loonkost voor één en alle functies te bereken toegevoegd, methode om functie toe te voegen of te verwijderen toegevoegd

--- src/KairosWeb_Groep6/Models/Domain/KostenBatenBeheer.cs
+++ src/KairosWeb_Groep6/Models/Domain/KostenBatenBeheer.cs
@@ -1,17 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace KairosWeb_Groep6.Models.Domain
 {
     public class KostenBatenBeheer
     {
-        private List<KostOfBaat> kostenEnBaten { get; set; }
+        private IList<KostOfBaat> KostenEnBaten { get; set; }
 
-        private List<Functie> functies { get; set; }
+        private IList<Functie> Functies { get; }
         
         public KostenBatenBeheer()
         {
+            Functies = new List<Functie>();
             throw new NotImplementedException();
-        }  
+        }
+
+        public double GeefLoonkostVoorFunctieMetNaam(string naam)
+        {
+            Functie functie = Functies.SingleOrDefault(f => f.Naam.Equals(naam));
+            
+            if (functie != null)
+            {
+                return functie.BerekenTotaleLoonkost();
+            }
+
+            return 0;
+        }
+
+        public double GeefTotaleLoonkostAlleFunties()
+        {
+            double somAlleLoonkosten = Functies.Sum(f => f.BerekenTotaleLoonkost());
+            return somAlleLoonkosten;
+        }
+
+        public void VoegFunctieToe(Functie functie)
+        {
+            Functies.Add(functie);
+        }
+
+        public void VerwijderFunctie(Functie functie)
+        {
+            Functies.Remove(functie);
+        }
     }
 }
