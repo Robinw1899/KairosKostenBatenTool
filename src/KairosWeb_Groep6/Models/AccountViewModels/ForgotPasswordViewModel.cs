5de4ba4eab42b69b5baeb627e60759873c6687c6
Thomas Aelbrecht <thomasaelbrecht@live.com>
2017-03-10 23:46:24 +0100

    Wachtwoord vergeten email verzenden afgewerkt

--- src/KairosWeb_Groep6/Models/AccountViewModels/ForgotPasswordViewModel.cs
+++ src/KairosWeb_Groep6/Models/AccountViewModels/ForgotPasswordViewModel.cs
@@ -4,8 +4,8 @@ namespace KairosWeb_Groep6.Models.AccountViewModels
 {
     public class ForgotPasswordViewModel
     {
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "Gelieve je emailadres op te geven")]
+        [EmailAddress(ErrorMessage = "Gelieve een geldig emailadres op te geven")]
         public string Email { get; set; }
     }
 }
