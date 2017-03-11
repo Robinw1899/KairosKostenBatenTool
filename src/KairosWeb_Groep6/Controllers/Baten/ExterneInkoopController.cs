67c8e1a094afb270d8f294118d4267d16e9b5dde
DimmyMaenhout <DimmyMaenhout@DIMMYMAENHOD09C>
2017-03-11 18:36:05 +0100

    baten controllers aangemaakt

---
+++ src/KairosWeb_Groep6/Controllers/Baten/ExterneInkoopController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KairosWeb_Groep6.Filters;
+using KairosWeb_Groep6.Models.Domain;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KairosWeb_Groep6.Controllers.Baten
+{
+    [ServiceFilter(typeof(AnalyseFilter))]
+    public class ExterneInkoopController : Controller
+    {
+        private readonly IAnalyseRepository _analyseRepository;
+
+        public ExterneInkoopController(IAnalyseRepository analyseRepository)
+        {
+            _analyseRepository = analyseRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index(Analyse analyse)
+        {
+            ExterneInkoopViewModel model = new ExterneInkoopViewModel();
+            if (IsAjaxRequest())
+            {
+
+            }
+            return View();
+        }
+
+        private bool IsAjaxRequest()
+        {
+            return Request != null && Request.Headers["X-Requested-With"] == "XMLHttpRequest";
+        }
+    }
+}
