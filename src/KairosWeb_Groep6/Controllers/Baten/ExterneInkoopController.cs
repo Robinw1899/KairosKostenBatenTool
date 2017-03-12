36cbf74d2257c251e65ece97a8a96ec196e217e2
DimmyMaenhout <DimmyMaenhout@DIMMYMAENHOD09C>
2017-03-12 08:58:25 +0100

    Controllers aangepast (code toegevoegd)

--- src/KairosWeb_Groep6/Controllers/Baten/ExterneInkoopController.cs
+++ src/KairosWeb_Groep6/Controllers/Baten/ExterneInkoopController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using KairosWeb_Groep6.Filters;
 using KairosWeb_Groep6.Models.Domain;
+using KairosWeb_Groep6.Models.Domain.Baten;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -29,7 +30,47 @@ namespace KairosWeb_Groep6.Controllers.Baten
             }
             return View();
         }
+        public IActionResult VoegToe(Analyse analyse, ExterneInkoopViewModel model /*UitzendKrachtBesparingIndexViewModel model*/)
+        {
+            if (ModelState.IsValid)
+            {
+                // de baat bestaat reeds:
+                ExterneInkoop baat = new ExterneInkoop
+                {
+                    //Id = model.Id,
+                    Id = 1,
+                    Type = model.Type,
+                    Soort = model.Soort,
+                    Beschrijving = model.Beschrijving,
+                    Bedrag = model.Bedrag
+                };
 
+                analyse.ExterneInkopen.Add(baat);
+                _analyseRepository.Save();
+
+                /*model = MaakModel(analyse);
+                PlaatsTotaalInViewData(analyse);*/
+
+                return PartialView("_OverzichtTabel", model.ViewModels);
+            }
+
+           /* PlaatsTotaalInViewData(analyse);*/
+
+            return RedirectToAction("Index", model);
+        }
+        private ExterneInkoopIndexViewModel MaakModel(Analyse analyse)
+        {
+            ExterneInkoopIndexViewModel model = new ExterneInkoopIndexViewModel
+            {
+                Type = Models.Domain.Type.Baat,
+                Soort = Soort.MedewerkersHogerNiveau,
+                ViewModels = analyse
+                                .MedewerkersHogerNiveauBaat
+                                .Select(m => new ExterneInkoopViewModel(m))
+            };
+
+            return model;
+        }
         private bool IsAjaxRequest()
         {
             return Request != null && Request.Headers["X-Requested-With"] == "XMLHttpRequest";
--- src/KairosWeb_Groep6/Controllers/Baten/ExtraOmzetController.cs
+++ src/KairosWeb_Groep6/Controllers/Baten/ExtraOmzetController.cs
@@ -3,17 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using KairosWeb_Groep6.Filters;
+using KairosWeb_Groep6.Models.Domain;
+using KairosWeb_Groep6.Models.Domain.Baten;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace KairosWeb_Groep6.Controllers.Baten
 {
+    [ServiceFilter(typeof(AnalyseFilter))]
     public class ExtraOmzetController : Controller
     {
+
+        private readonly IAnalyseRepository _analyseRepository;
+
+        public ExtraOmzetController(IAnalyseRepository analyseRepository)
+        {
+            _analyseRepository = analyseRepository;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult VoegToe(Analyse analyse, ExtraOmzetIndexViewModel model /*UitzendKrachtBesparingIndexViewModel model*/)
+        {
+            if (ModelState.IsValid)
+            {
+                // de baat bestaat reeds:
+                ExtraOmzet baat = new ExtraOmzet
+                {
+                    //Id = model.Id,
+                    Id = 1,
+                    Type = model.Type,
+                    Soort = model.Soort,
+                    Beschrijving = model.Beschrijving,
+                    Bedrag = model.Bedrag
+                };
+
+                analyse.ExtraOmzet.Add(baat);
+                _analyseRepository.Save();
+
+                /*model = MaakModel(analyse);
+                PlaatsTotaalInViewData(analyse);*/
+
+                return PartialView("_OverzichtTabel", model.ViewModels);
+            }
+
+            /* PlaatsTotaalInViewData(analyse);*/
+
+            return RedirectToAction("Index", model);
+        }
+        private ExtraOmzetIndexViewModel MaakModel(Analyse analyse)
+        {
+            ExtraOmzetIndexViewModel model = new ExtraOmzetIndexViewModel
+            {
+                Type = Models.Domain.Type.Baat,
+                Soort = Soort.MedewerkersHogerNiveau,
+                ViewModels = analyse
+                                .MedewerkersHogerNiveauBaat
+                                .Select(m => new ExtraOmzetViewModel(m))
+            };
+
+            return model;
+        }
+        private bool IsAjaxRequest()
+        {
+            return Request != null && Request.Headers["X-Requested-With"] == "XMLHttpRequest";
         }
     }
 }
