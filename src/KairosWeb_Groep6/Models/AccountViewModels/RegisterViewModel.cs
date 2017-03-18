291a856fc71499d3006a3fa29fe7c26c20e4e3e9
robin <robin@ROBIN>
2017-03-18 18:33:28 +0100

    Datatype Emailaddress in comment zetten

--- src/KairosWeb_Groep6/Models/AccountViewModels/RegisterViewModel.cs
+++ src/KairosWeb_Groep6/Models/AccountViewModels/RegisterViewModel.cs
@@ -18,7 +18,7 @@ namespace KairosWeb_Groep6.Models.AccountViewModels
         public string Voornaam { get; set; }
 
         [Required(ErrorMessage = "Emailadres is verplicht")]
-        [DataType(DataType.EmailAddress)]
+       // [DataType(DataType.EmailAddress)]
         [Display(Name = "Emailadres")]
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}", ErrorMessage = "Een email addres moet bestaan uit een '@' gevolgd door een '.'")]
         public string Email { get; set; }
