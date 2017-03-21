0d315ef59c93f4abce18cc21011908199b7ee3fc
DimmyMaenhout <DimmyMaenhout@DIMMYMAENHOD09C>
2017-03-21 21:36:53 +0100

    Data annotaties bijgezet

--- src/KairosWeb_Groep6/Models/KairosViewModels/Kosten/LoonKostViewModels/LoonKostIndexViewModel.cs
+++ src/KairosWeb_Groep6/Models/KairosViewModels/Kosten/LoonKostViewModels/LoonKostIndexViewModel.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using KairosWeb_Groep6.Models.Domain.Kosten;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KairosWeb_Groep6.Models.KairosViewModels.Kosten.LoonKostViewModels
 {
     public class LoonKostIndexViewModel
     {
+        [HiddenInput]
         public int Id { get; set; }//dit toegevoegd
+        [Required(ErrorMessage = "Gelieve een (korte) beschrijving van de functie op te geven.")]
         public String Beschrijving { get; set; }
+        [Required(ErrorMessage = "Gelieve een het aantal uren per week op te geven.")]
         public double AantalUrenPerWeek { get; set; }
 
         public double Bedrag // = kolom "totale loonkost eerste jaar"
@@ -20,15 +25,15 @@ namespace KairosWeb_Groep6.Models.KairosViewModels.Kosten.LoonKostViewModels
             }
             set { } // setter wordt nooit gebruikt
         }
-
+        [Required(ErrorMessage = "Gelieve het bruto maandloon (fulltime) op te geven.")]
         public double BrutoMaandloonFulltime { get; set; }
-
+        [Required(ErrorMessage = "Gelieve de ondersteuningspremie in te vullen.")]
         public double Ondersteuningspremie { get; set; }
-
+        [Required(ErrorMessage = "Gelieve het aantal maanden IBO in te vullen.")]
         public int AantalMaandenIBO { get; set; }
-
+        [Required(ErrorMessage = "Gelieve de IBO premie in te vullen.")]
         public double IBOPremie { get; set; }
-
+        [Required(ErrorMessage = "Gelieve een doelgroep op te geven.")]
         public Doelgroep? Doelgroep { get; set; }
         public IEnumerable<LoonkostViewModel> ViewModels { get; internal set; }
 
--- src/KairosWeb_Groep6/Models/KairosViewModels/Kosten/LoonKostViewModels/LoonkostViewModel.cs
+++ src/KairosWeb_Groep6/Models/KairosViewModels/Kosten/LoonKostViewModels/LoonkostViewModel.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using KairosWeb_Groep6.Models.Domain.Kosten;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KairosWeb_Groep6.Models.KairosViewModels.Kosten
 {
     public class LoonkostViewModel
     {
 
+        [HiddenInput]
         public int Id { get; set; }//dit toegevoegd
-
+        [Required(ErrorMessage = "Gelieve een (korte) beschrijving van de functie op te geven.")]
         public String Beschrijving { get; set; }
+        [Required(ErrorMessage = "Gelieve een het aantal uren per week op te geven.")]
         public double AantalUrenPerWeek { get; set; }
 
         public double Bedrag // = kolom "totale loonkost eerste jaar"
@@ -22,17 +26,17 @@ namespace KairosWeb_Groep6.Models.KairosViewModels.Kosten
             }
             set { } // setter wordt nooit gebruikt
         }
-
+        [Required(ErrorMessage = "Gelieve het bruto maandloon (fulltime) op te geven.")]
         public double BrutoMaandloonFulltime { get; set; }
-
+        [Required(ErrorMessage = "Gelieve de ondersteuningspremie in te vullen.")]
         public double Ondersteuningspremie { get; set; }
-
+        [Required(ErrorMessage = "Gelieve het aantal maanden IBO in te vullen.")]
         public int AantalMaandenIBO { get; set; }
-
+        [Required(ErrorMessage = "Gelieve de IBO premie in te vullen.")]
         public double IBOPremie { get; set; }
-
+        [Required(ErrorMessage = "Gelieve een doelgroep op te geven.")]
         public Doelgroep? Doelgroep { get; set; }
-
+        public IEnumerable<LoonkostViewModel> ViewModels { get; internal set; }
 
         public LoonkostViewModel()
         {
