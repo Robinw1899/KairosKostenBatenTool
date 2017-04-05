a2530a7ab78ccff1f2563d9ad43bd3f726d87a00
Thomas Aelbrecht <thomas.aelbrecht.w4356@student.hogent.be>
2017-04-05 14:03:52 +0200

    Controller aangemaakt + 4 NotImplemented-methoden aangemaakt

---
+++ src/KairosWeb_Groep6/Controllers/ArchiefController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using KairosWeb_Groep6.Filters;
+using KairosWeb_Groep6.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KairosWeb_Groep6.Controllers
+{
+    [ServiceFilter(typeof(JobcoachFilter))]
+    public class ArchiefController : Controller
+    {
+        #region Properties
+
+        private readonly IJobcoachRepository _jobcoachRepository;
+        #endregion
+
+        #region Constructors
+        public ArchiefController(IJobcoachRepository jobcoachRepository)
+        {
+            _jobcoachRepository = jobcoachRepository;
+        }
+        #endregion
+
+        #region Methods
+        public IActionResult Index(Jobcoach jobcoach)
+        {
+            if (jobcoach != null)
+            {
+                
+            }
+
+            return View();
+        }
+
+        public IActionResult HaalAnalyseUitArchief()
+        {
+            
+        }
+
+        public IActionResult OpenAnalyse()
+        {
+
+        }
+
+        public IActionResult VerwijderAnalyse()
+        {
+
+        }
+
+        public IActionResult MaakExcelAnalyse()
+        {
+            throw new NotImplementedException("Archief/MaakExcelAnalyse");
+        }
+
+        public IActionResult MaakPdfAnalyse()
+        {
+            throw new NotImplementedException("Archief/MaakPdfAnalyse");
+        }
+
+        public IActionResult AfdrukkenAnalyse()
+        {
+            throw new NotImplementedException("Archief/AfdrukkenAnalyse");
+        }
+
+        public IActionResult MailAnalyse()
+        {
+            throw new NotImplementedException("Archief/MailAnalyse");
+        }
+        #endregion
+    }
+}
