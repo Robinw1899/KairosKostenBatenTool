f8ea50506d771fc11afbdf564d291da55c88b5e7
Thomas Aelbrecht <thomas.aelbrecht.w4356@student.hogent.be>
2017-04-08 15:13:05 +0200

    PasswordGeneratorTest geïmplementeerd

---
+++ test/KairosWeb_Groep6.Tests/Models/Domain/PasswordGeneratorTest.cs
@@ -0,0 +1,20 @@
+﻿using KairosWeb_Groep6.Models.Domain;
+using Xunit;
+
+namespace KairosWeb_Groep6.Tests.Models.Domain
+{
+    public class PasswordGeneratorTest
+    {
+        [Theory]
+        [InlineData(2)]
+        [InlineData(6)]
+        [InlineData(10)]
+        [InlineData(16)]
+        public void TestGeneratePassword(int lengte)
+        {
+            string password = PasswordGenerator.GeneratePassword(lengte);
+
+            Assert.Equal(lengte, password.Length);
+        }
+    }
+}
