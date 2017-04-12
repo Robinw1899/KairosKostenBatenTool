bd78abdeb4992d1919ec391fcc81b45cb2aa18d5
robin <robin@ROBIN>
2017-04-12 16:19:11 +0200

    Maken van een ContactpersoonController BestaandeContactPersoonViewModel maken, en de Index.cshtml maken

---
+++ src/KairosWeb_Groep6/Controllers/ContactPersoonController.cs
@@ -0,0 +1,47 @@
+﻿using KairosWeb_Groep6.Models.Domain;
+using KairosWeb_Groep6.Models.KairosViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KairosWeb_Groep6.Controllers
+{
+    public class ContactPersoonController : Controller
+    {
+        #region properties
+        private readonly IAnalyseRepository _analyseRepository;
+        private readonly IDepartementRepository _departementRepository;
+        private readonly IWerkgeverRepository _werkgeverRepository;
+        #endregion 
+
+        #region constructor
+        public ContactPersoonController(
+            IAnalyseRepository analyseRepository,
+            IDepartementRepository departementenRepository,
+            IWerkgeverRepository werkgeverRepository)
+        {
+            _analyseRepository = analyseRepository;
+            _departementRepository = departementenRepository;
+            _werkgeverRepository = werkgeverRepository;
+        }
+        #endregion
+
+        public IActionResult Index(int WerkgeverId)
+        {
+            Werkgever werkgever = _werkgeverRepository.GetById(WerkgeverId);
+            IEnumerable<ContactPersoon> contactpersonen = werkgever.ContactPersonen;
+
+
+            BestaandeContactPersoonViewModel model = new BestaandeContactPersoonViewModel
+            {
+                ContactPersonen = contactpersonen.Select(w => new ContactPersoonViewModel(w,WerkgeverId))
+                                       .ToList()
+            };
+
+            return View(model);
+
+        }      
+    }
+}
---
+++ src/KairosWeb_Groep6/Models/KairosViewModels/BestaandeContactPersoonViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KairosWeb_Groep6.Models.KairosViewModels
+{
+    public class BestaandeContactPersoonViewModel
+    {
+        [HiddenInput]
+        public int WerkgeverId { get; set; }
+
+        public IEnumerable<ContactPersoonViewModel> ContactPersonen { get; set; }
+    }
+}
