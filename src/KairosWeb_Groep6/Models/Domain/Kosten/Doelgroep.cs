45dc55a0d0aa5de9804a41be05899b017670235e
Thomas Aelbrecht <thomasaelbrecht@live.com>
2017-04-24 09:03:36 +0200

    DoelgroepSoortExtensions verwijderd

--- src/KairosWeb_Groep6/Models/Domain/Kosten/Doelgroep.cs
+++ src/KairosWeb_Groep6/Models/Domain/Kosten/Doelgroep.cs
@@ -1,6 +1,4 @@
-﻿using KairosWeb_Groep6.Models.Domain.Extensions;
-
-namespace KairosWeb_Groep6.Models.Domain.Kosten
+﻿namespace KairosWeb_Groep6.Models.Domain.Kosten
 {
     public class Doelgroep
     {
@@ -9,6 +7,9 @@ namespace KairosWeb_Groep6.Models.Domain.Kosten
 
         // het minimum brutoloon dat hoort bij deze doelgroep, dit kan veranderen!
         public decimal MinBrutoloon { get; set; }
+
+        // de doelgroepvermindering die hoort bij deze doelgroep, dit kan veranderen!
+        public decimal DoelgroepVermindering { get; set; }
         #endregion
 
         #region Constructors
@@ -24,10 +25,82 @@ namespace KairosWeb_Groep6.Models.Domain.Kosten
         }
         #endregion
 
+        #region Controleermethoden
+        private bool ControleerGegevensOntbreken(decimal brutoloon, decimal aantalUrenPerWeek, decimal aantalWerkuren, decimal patronaleBijdrage)
+        {
+            if (aantalWerkuren <= 0)
+            {
+                return false;
+            }
+
+            if (patronaleBijdrage <= 0)
+            {
+                return false;
+            }
+
+            if (brutoloon <= 0)
+            {
+                return false;
+            }
+
+            if (aantalUrenPerWeek <= 0)
+            {
+                return false;
+            }
+
+            if (MinBrutoloon <= 0)
+            {
+                return false;
+            }
+
+            if (DoelgroepVermindering <= 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+
         #region Methods
+        public decimal BerekenDoelgroepVermindering(decimal brutoloon, decimal aantalUrenPerWeek, 
+            decimal aantalWerkuren, decimal patronaleBijdrage)
+        {
+            if (ControleerGegevensOntbreken(brutoloon, aantalUrenPerWeek, aantalWerkuren, patronaleBijdrage))
+            {
+                // ALS bruto maandloon < minBrutoloon:
+                // ((BEDRAG DOELGROEP / aantal uur voltijdse werkweek) *aantal uur dat medewerker werkt)/ 4
+                decimal doelgroepvermindering = 0;
+
+                if (brutoloon < MinBrutoloon)
+                {
+                    decimal verminderingPerUurWerkweek = DoelgroepVermindering / aantalWerkuren;
+                    doelgroepvermindering = (verminderingPerUurWerkweek * aantalUrenPerWeek) / 4;
+                }
+
+                return doelgroepvermindering;
+            }
+
+            return 0;
+        }
+
         public override string ToString()
         {
-            return DoelgroepSoortExtensions.GeefOmschrijving(Soort);
+            switch (Soort)
+            {
+                case DoelgroepSoort.LaaggeschooldTot25:
+                    return "Wn's < 25 jaar laaggeschoold";
+                case DoelgroepSoort.MiddengeschooldTot25:
+                    return "Wn's < 25 jaar middengeschoold";
+                case DoelgroepSoort.Tussen55En60:
+                    return "Wn's ≥ 55 en < 60 jaar";
+                case DoelgroepSoort.Vanaf60:
+                    return "Wns ≥ 60 jaar";
+                case DoelgroepSoort.Andere:
+                    return "Andere";
+                default:
+                    return "";
+            }
         }
         #endregion
     }
