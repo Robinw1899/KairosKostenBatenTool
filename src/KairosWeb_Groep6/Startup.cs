d4a969d2f3fbd1606450b21d55fce0c4b5b0f279
Thomas Aelbrecht <thomasaelbrecht@live.com>
2017-05-03 14:15:55 +0200

    WerkgeverControllerTest afgewerkt

--- src/KairosWeb_Groep6/Startup.cs
+++ src/KairosWeb_Groep6/Startup.cs
@@ -121,10 +121,10 @@ namespace KairosWeb_Groep6
                     template: "{controller=Kairos}/{action=Index}/{id?}");
             });
 
-            //DataInitializer initializer = new DataInitializer(context, userManager, gebruikerRepository,
-            //                                                  departementRepository, analyseRepository, werkgeverRepository,
-            //                                                 introductietekstRepository, doelgroepRepository);
-            //initializer.InitializeData().Wait();
+            DataInitializer initializer = new DataInitializer(context, userManager, gebruikerRepository,
+                                                              departementRepository, analyseRepository, werkgeverRepository,
+                                                             introductietekstRepository, doelgroepRepository);
+            initializer.InitializeData().Wait();
         }
 
         private RequestLocalizationOptions BuildLocalizationOptions()
--- test/KairosWeb_Groep6.Tests/Controllers/WerkgeverControllerTest.cs
+++ test/KairosWeb_Groep6.Tests/Controllers/WerkgeverControllerTest.cs
@@ -1,6 +1,10 @@
-﻿using KairosWeb_Groep6.Controllers;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KairosWeb_Groep6.Controllers;
 using KairosWeb_Groep6.Models.Domain;
 using KairosWeb_Groep6.Models.KairosViewModels;
+using KairosWeb_Groep6.Tests.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Moq;
@@ -15,6 +19,8 @@ namespace KairosWeb_Groep6.Tests.Controllers
         private readonly Mock<IDepartementRepository> _departementRepository;
         private readonly Mock<IWerkgeverRepository> _werkgeverRepository;
         private readonly WerkgeverController _controller;
+        private readonly Mock<Analyse> _analyse;
+        private readonly DummyApplicationDbContext _dbContext;
         #endregion
 
         #region Constructors
@@ -27,34 +33,62 @@ namespace KairosWeb_Groep6.Tests.Controllers
             _controller = new WerkgeverController(_analyseRepository.Object,
                 _departementRepository.Object, _werkgeverRepository.Object);
             _controller.TempData = new Mock<ITempDataDictionary>().Object;
+
+            _analyse = new Mock<Analyse>();
+            _dbContext = new DummyApplicationDbContext();
         }
         #endregion
 
         #region Index
-        [Fact(Skip = "Not implemented yet")]
+        [Fact]
         public void TestIndex_DepartementNull_RedirectsToSelecteerWerkgever()
         {
-            
+            var result = _controller.Index(new Analyse()) as RedirectToActionResult;
+
+            Assert.Equal("SelecteerWerkgever", result?.ActionName);
         }
 
-        [Fact(Skip = "Not implemented yet")]
+        [Fact]
         public void TestIndex_Succes()
         {
+            Analyse analyse = new Analyse {Departement = _dbContext.Aldi};
 
+            var result = _controller.Index(analyse) as ViewResult;
+            var model = result?.Model as WerkgeverViewModel;
+
+            Assert.Equal(typeof(WerkgeverViewModel), result?.Model.GetType());
+            Assert.Equal("ALDI", model?.Naam);
         }
         #endregion
 
         #region Opslaan
-        [Fact(Skip = "Not implemented yet")]
-        public void TestOpslaan_RepositoryGooitException_MethodeFaaltNiet()
+        [Fact]
+        public void TestOpslaan_RepositoryGooitException_ReturnsViewWithModel()
         {
-            
+            _departementRepository.Setup(d => d.GetById(It.IsAny<int>())).Throws(new Exception());
+
+            WerkgeverViewModel model = new WerkgeverViewModel(_dbContext.Aldi);
+
+            var result = _controller.Opslaan(_analyse.Object, model) as ViewResult;
+            var resultModel = result?.Model as WerkgeverViewModel;
+
+            Assert.Equal(model, resultModel);
+            Assert.Equal("Index", result?.ViewName);
         }
 
-        [Fact(Skip = "Not implemented yet")]
+        [Fact]
         public void TestOpslaan_Succes()
         {
+            Analyse analyse = new Analyse { Departement = _dbContext.Aldi };
 
+            _departementRepository.Setup(d => d.GetById(It.IsAny<int>())).Returns(_dbContext.Aldi);
+
+            WerkgeverViewModel model = new WerkgeverViewModel(_dbContext.Aldi);
+
+            var result = _controller.Opslaan(analyse, model) as RedirectToActionResult;
+
+            Assert.Equal("Index", result?.ActionName);
+            Assert.Equal(_dbContext.Aldi, analyse.Departement);
         }
         #endregion
 
@@ -81,7 +115,278 @@ namespace KairosWeb_Groep6.Tests.Controllers
         #endregion
 
         #region Nieuwe werkgever -- POST --
-        // To do
+        [Fact]
+        public void TestNieuweWerkgever_RepositoryGooitException_MethodeFaaltNiet()
+        {
+            _departementRepository.Setup(r => r.Save()).Throws(new Exception());
+
+            WerkgeverViewModel model = new WerkgeverViewModel();
+
+            var result = _controller.NieuweWerkgever(_analyse.Object, model) as ViewResult;
+            var resultModel = result?.Model as WerkgeverViewModel;
+
+            Assert.Equal(model, resultModel);
+            Assert.Equal("NieuweWerkgever", result?.ViewName);
+        }
+
+        [Fact]
+        public void TestNieuweWerkgever_BestaandDepartement_ReturnsViewMetZelfdeModel()
+        {
+            _departementRepository.Setup(r => r.GetByName(It.IsAny<string>())).Returns(_dbContext.Aldi);
+
+            WerkgeverViewModel model = new WerkgeverViewModel(_dbContext.Aldi);
+
+            var result = _controller.NieuweWerkgever(_analyse.Object, model) as ViewResult;
+            var resultModel = result?.Model as WerkgeverViewModel;
+
+            Assert.Equal(model, resultModel);
+            Assert.Equal("NieuweWerkgever", result?.ViewName);
+        }
+
+        [Fact]
+        public void TestNieuweWerkgever_Succes_RedirectsToIndexContactPersoon()
+        {
+            _departementRepository.Setup(r => r.GetByName(It.IsAny<string>())).Returns(_dbContext.Aldi);
+
+            WerkgeverViewModel model = new WerkgeverViewModel
+            {
+                AantalWerkuren = 35M,
+                Bus = "",
+                Departement = "HR",
+                Gemeente = "Wieze",
+                Postcode = 9280,
+                Nummer = 42,
+                Straat = "Wolvenstraat",
+                PatronaleBijdrage = 35M,
+                Naam = "Thomas NV"
+            };
+
+            var result = _controller.NieuweWerkgever(_analyse.Object, model) as RedirectToActionResult;
+
+            Assert.Equal("Index", result?.ActionName);
+            Assert.Equal("ContactPersoon", result?.ControllerName);
+
+            _departementRepository.Verify(r => r.Add(It.IsAny<Departement>()), Times.Once);
+            _departementRepository.Verify(r => r.Save(), Times.Once);
+
+            _analyseRepository.Verify(r => r.Save(), Times.Once);
+        }
+        #endregion
+
+        #region Bestaande werkgever -- GET --
+        [Fact]
+        public void TestBestaandeWerkgeverGET_RepositoryGooitException_MethodeFaaltNiet()
+        {
+            _werkgeverRepository.Setup(r => r.GetAll()).Throws(new Exception());
+
+            var result = _controller.BestaandeWerkgever() as RedirectToActionResult;
+
+            Assert.Equal("Index", result?.ActionName);
+        }
+
+        [Fact]
+        public void TestBestaandeWerkgever_ReturnsViewWithModel()
+        {
+            _werkgeverRepository.Setup(r => r.GetAll())
+                .Returns(new List<Werkgever>
+                {
+                    _dbContext.Aldi.Werkgever
+                });
+
+            var result = _controller.BestaandeWerkgever() as ViewResult;
+            var model = result?.Model as BestaandeWerkgeverViewModel;
+
+            Assert.Equal(1, model?.Werkgevers.Count());
+        }
+        #endregion
+
+        #region Selecteer bestaande werkgever -- POST --
+        [Fact]
+        public void TestBestaandeWerkgever_RepositoryGooitException_MethodeFaaltNiet()
+        {
+            _analyseRepository.Setup(a => a.Save()).Throws(new Exception());
+
+            var result = _controller.SelecteerBestaandeWerkgever(_analyse.Object, 1, 1) as RedirectToActionResult;
+
+            Assert.Equal("BestaandeWerkgever", result?.ActionName);
+        }
+
+        [Fact]
+        public void TestBestaandeWerkgever_Succes_RedirectsToIndexContactPersoon()
+        {
+            _departementRepository.Setup(r => r.GetById(It.IsAny<int>())).Returns(_dbContext.Aldi);
+
+            var result = _controller.SelecteerBestaandeWerkgever(_analyse.Object, 1, 1) as RedirectToActionResult;
+
+            Assert.Equal("Index", result?.ActionName);
+            Assert.Equal("ContactPersoon", result?.ControllerName);
+
+            _analyseRepository.Verify(a => a.Save(), Times.Once);
+        }
+        #endregion
+
+        #region ZoekDepartement
+        [Fact]
+        public void TestZoekDepartement_RepositoryGooitException_RedirectsToBestaandDepartement()
+        {
+            _departementRepository.Setup(r => r.GetAllVanWerkgever(It.IsAny<int>())).Throws(new Exception());
+
+            var result = _controller.ZoekDepartementen(1, "hallo") as RedirectToActionResult;
+
+            Assert.Equal("BestaandDepartement", result?.ActionName);
+        }
+
+        [Fact]
+        public void TestZoekDepartement_ReturnsPartial()
+        {
+            _departementRepository.Setup(r => r.GetAllVanWerkgever(It.IsAny<int>()))
+                .Returns(new List<Departement>());
+
+            var result = _controller.ZoekDepartementen(1, "geen") as PartialViewResult;
+
+            Assert.Equal("_Departementen", result?.ViewName);
+        }
+        #endregion
+
+        #region ZoekWerkgever
+        [Fact]
+        public void TestZoekWerkgever_RepositoryGooitException_RedirectsToBestaandeWerkgever()
+        {
+            _werkgeverRepository.Setup(r => r.GetByName(It.IsAny<string>())).Throws(new Exception());
+
+            var result = _controller.ZoekWerkgever("hallo") as RedirectToActionResult;
+
+            Assert.Equal("BestaandeWerkgever", result?.ActionName);
+        }
+
+        [Fact]
+        public void TestZoekWerkgever_NaamNull_MethodeFaaltNiet()
+        {
+            _werkgeverRepository.Setup(r => r.GetAll())
+                .Returns(new List<Werkgever>());
+
+            var result = _controller.ZoekWerkgever(null) as PartialViewResult;
+
+            Assert.Equal("_Werkgevers", result?.ViewName);
+        }
+
+        [Fact]
+        public void TestZoekWerkgever_NaamEmpty_MethodeFaaltNiet()
+        {
+            _werkgeverRepository.Setup(r => r.GetAll())
+                .Returns(new List<Werkgever>());
+
+            var result = _controller.ZoekWerkgever("") as PartialViewResult;
+
+            Assert.Equal("_Werkgevers", result?.ViewName);
+        }
+
+        [Fact]
+        public void TestZoekWerkgever_ReturnsPartial()
+        {
+            _werkgeverRepository.Setup(r => r.GetAll())
+                .Returns(new List<Werkgever>());
+
+            var result = _controller.ZoekWerkgever("geen") as PartialViewResult;
+
+            Assert.Equal("_Werkgevers", result?.ViewName);
+        }
+        #endregion
+
+        #region Bestaand departement
+        [Fact]
+        public void TestBestaandDepartement_RepositoryGooitException_RedirectsToBestaandeWerkgever()
+        {
+            _departementRepository.Setup(d => d.GetAllVanWerkgever(It.IsAny<int>())).Throws(new Exception());
+
+            var result = _controller.BestaandDepartement(1) as RedirectToActionResult;
+
+            Assert.Equal("BestaandeWerkgever", result?.ActionName);
+        }
+
+        [Fact]
+        public void TestBestaandDepartement_Succes()
+        {
+            _departementRepository.Setup(d => d.GetAllVanWerkgever(It.IsAny<int>()))
+                .Returns(new List<Departement>
+                {
+                    _dbContext.Aldi
+                });
+
+            var result = _controller.BestaandDepartement(1) as ViewResult;
+            var model = result?.Model as BestaandDepartementViewModel;
+
+            Assert.Equal(1, model?.WerkgeverId);
+            Assert.Equal(1, model?.Departementen.Count());
+        }
+        #endregion
+
+        #region Nieuw departement -- GET --
+        [Fact]
+        public void TestNieuwDepartementGET_RepositoryGooitException_RedirectsToBestaandeWerkgever()
+        {
+            _werkgeverRepository.Setup(d => d.GetById(It.IsAny<int>())).Throws(new Exception());
+
+            var result = _controller.NieuwDepartement(1) as RedirectToActionResult;
+
+            Assert.Equal("BestaandeWerkgever", result?.ActionName);
+        }
+
+        [Fact]
+        public void TestNieuwDepartementGET_Succes()
+        {
+            _werkgeverRepository.Setup(d => d.GetById(It.IsAny<int>()))
+                .Returns(_dbContext.Aldi.Werkgever);
+
+            var result = _controller.NieuwDepartement(1) as ViewResult;
+            var model = result?.Model as WerkgeverViewModel;
+
+            Assert.Equal(0, model?.WerkgeverId);
+            Assert.Equal("ALDI", model?.Naam);
+        }
+        #endregion
+
+        #region Nieuw departement -- POST --
+        [Fact]
+        public void TestNieuwDepartementPOST_RepositoryGooitException_ReturnsView()
+        {
+            _departementRepository.Setup(d => d.GetByName(It.IsAny<string>())).Throws(new Exception());
+
+            var result = _controller.NieuwDepartement(_analyse.Object, new WerkgeverViewModel()) as ViewResult;
+
+            Assert.Equal(typeof(WerkgeverViewModel), result?.Model.GetType());
+        }
+
+        [Fact]
+        public void TestNieuwDepartementPOST_BestaandDepartement_ReturnsView()
+        {
+            _departementRepository.Setup(d => d.GetByName(It.IsAny<string>()))
+                .Returns(_dbContext.Aldi);
+
+            WerkgeverViewModel model = new WerkgeverViewModel(_dbContext.Aldi);
+
+            var result = _controller.NieuwDepartement(_analyse.Object, model) as ViewResult;
+            var resultModel = result?.Model as WerkgeverViewModel;
+
+            Assert.Equal(typeof(WerkgeverViewModel), resultModel?.GetType());
+            Assert.Equal(0, resultModel?.WerkgeverId);
+            Assert.Equal("ALDI", resultModel?.Naam);
+            Assert.Equal("NieuwDepartement", result?.ViewName);
+        }
+
+        [Fact]
+        public void TestNieuwDepartementPOST_Succes()
+        {
+            _werkgeverRepository.Setup(d => d.GetById(It.IsAny<int>()))
+                .Returns(_dbContext.Aldi.Werkgever);
+
+            WerkgeverViewModel model = new WerkgeverViewModel(_dbContext.Aldi);
+
+            var result = _controller.NieuwDepartement(_analyse.Object, model) as RedirectToActionResult;
+
+            Assert.Equal("Index", result?.ActionName);
+            Assert.Equal("Resultaat", result?.ControllerName);
+        }
         #endregion
     }
 }
